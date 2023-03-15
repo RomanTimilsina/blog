260f753987c915345b802f069bc66b626bb2312f
Roman <romantimilsina@gmail.com>
2023-03-15 20:35:01 +0545

    jwt_authentication_with_login_and_logout

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
 import  express  from "express";
 import postRoute from './routes/posts.js'
+import usersRoute from './routes/users.js'
+import authRoute from './routes/auth.js'
+import cookieParser from "cookie-parser";
 
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
 app.use('/api/posts',postRoute)
+app.use('/api/users',usersRoute)
+app.use('/api/auth',authRoute)
 
 app.get('/test',(req,res) => {
   res.json("It works")
