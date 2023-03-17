ec2b9bf3d808c743956404526f22ed3a63c573ee
Roman <romantimilsina@gmail.com>
2023-03-17 15:28:39 +0545

    fetch_recommended_posts

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,17 +1,24 @@
  import { db } from "../db.js"
+ import jwt from 'jsonwebtoken'
 
 export const getPosts = (req,res) => {
   const q = req.query.cat ? "SELECT * FROM posts WHERE cat = ?" : "SELECT * FROM posts"
 
   db.query(q, [req.query.cat], (err, data) => {
-    if (err) return res.json(err)
+    if (err) return res.status(500).json(err)
 
     return res.status(200).json(data)
   })
 }
 
 export const getPost = (req,res) => {
-  res.json("from controller")
+  const q = 'SELECT `user`,`title`,`desc`,p.img,`cat`,`date` FROM users u JOIN posts p ON u.id=p.uid WHERE p.id = ?'
+
+  db.query(q,[req.params.id],(err,data) => {
+    if(err) return res.status(500).json(err)
+
+    return res.status(200).json(data[0])
+  }) 
 }
 
 export const addPost = (req,res) => {
@@ -19,7 +26,21 @@ export const addPost = (req,res) => {
 }
 
 export const deletePost = (req,res) => {
-  res.json("from controller")
+  const token = req.cookies.access_token
+  if(!token) return res.status(401).json("Not authenticated!")
+
+  jwt.verify(token,'jwtkey',(err, userInfo) => {
+    if(err) return res.status(403).json("Token is not valid")
+
+    const postId = req.params.id
+    const q = 'DELETE FROM posts WHERE `id` = ? AND `uid`= ?'
+
+    db.query(q,[postId,userInfo.id], (err,data) => {
+      if(err) return res.status(403).json("You can delete only your post")
+
+      return res.json("Your post has been deleted!")
+    })
+  })
 }
 
 export const updatePost = (req,res) => {
